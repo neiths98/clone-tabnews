60fa05d6d78a2eb719b47293fcb06a27d1ab1ef1
Nathan <nathanatrix@gmail.com>
2026-04-15 10:21:33 -0300

    feat: use `next-connect` in `/status` and `/migrations`

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,50 +1,58 @@
+import { createRouter } from 'next-connect'
 import database from 'infra/database'
 import migrationsRunner from 'node-pg-migrate'
 import { resolve } from 'node:path'
+import controller from 'infra/controller'
 
-async function migrations(req, res) {
-  const allowedMethods = ['GET', 'POST']
-  if (!allowedMethods.includes(req.method)) {
-    return res.status(405).json({ error: `Method ${req.method} not allowed` })
-  }
+const router = createRouter()
 
+router.get(getHandler)
+router.post(postHandler)
+
+export default router.handler(controller.errorHandlers)
+
+const defaultConfig = {
+  databaseUrl: process.env.DATA_TEST,
+  dir: resolve('infra', 'migrations'),
+  dryRun: true,
+  direction: 'up',
+  verbose: true,
+  migrationsTable: 'pgmigrations',
+}
+
+async function getHandler(req, res) {
   let dbClient
   try {
     dbClient = await database.getNewClient()
 
-    const defaultConfig = {
-      dbClient: dbClient,
-      databaseUrl: process.env.DATA_TEST,
-      dir: resolve('infra', 'migrations'),
-      dryRun: true,
-      direction: 'up',
-      verbose: true,
-      migrationsTable: 'pgmigrations',
-    }
+    const pendingMigrations = await migrationsRunner({
+      ...defaultConfig,
+      dbClient,
+    })
 
-    if (req.method === 'GET') {
-      const pendingMigrations = await migrationsRunner(defaultConfig)
-      return res.status(200).json(pendingMigrations)
-    }
-
-    if (req.method === 'POST') {
-      const migratedMigrations = await migrationsRunner({
-        ...defaultConfig,
-        dryRun: false,
-      })
-
-      if (migratedMigrations.length > 0) {
-        return res.status(201).json(migratedMigrations)
-      }
-
-      return res.status(200).json(migratedMigrations)
-    }
-  } catch (error) {
-    console.error(error)
-    throw error
+    return res.status(200).json(pendingMigrations)
   } finally {
     await dbClient.end()
   }
 }
 
-export default migrations
+async function postHandler(req, res) {
+  let dbClient
+  try {
+    dbClient = await database.getNewClient()
+
+    const migratedMigrations = await migrationsRunner({
+      ...defaultConfig,
+      dryRun: false,
+      dbClient,
+    })
+
+    if (migratedMigrations.length > 0) {
+      return res.status(201).json(migratedMigrations)
+    }
+
+    return res.status(200).json(migratedMigrations)
+  } finally {
+    await dbClient.end()
+  }
+}
